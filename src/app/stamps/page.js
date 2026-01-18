ac5f36aacb1f9c59164ecf324b48847f0785e13d
Char B <charburton18@gmail.com>
2026-01-17 17:28:03 -0700

    add /stamps page and global css variables

---
+++ src/app/stamps/page.js
@@ -0,0 +1,69 @@
+export default function Stamps() {
+  return (
+  <>
+    <h1 className='text-center'>My Stamps</h1>
+    <div className="grid grid-cols-2 gap-6 px-4">
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+      <div
+        className="w-40 h-40 rounded-full border-2 border-dotted flex items-center justify-center text-lg"
+        style={{ borderColor: '#C09635' }}
+      >
+      </div>
+    </div>
+  </>
+  );
+}
